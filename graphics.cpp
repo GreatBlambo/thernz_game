4165b2a05c06acbc3ee5c08870fd8e6945404254
Ray Chen <rayruichen@gmail.com>
2016-09-25 17:06:03 +0000

    fixed opengl ver

--- graphics.cpp
+++ graphics.cpp
@@ -232,8 +232,8 @@ GameError init_graphics(Graphics* graphics, WindowParams* window_params)
   // SDL_GL_CONTEXT_CORE gives us only the newer version, deprecated functions are disabled
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
  
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
   
   // Turn on double buffering with a 24bit Z buffer.
   // You may need to change this to 16 or 32 for your system
